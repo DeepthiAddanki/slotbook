c9cd0c2d37eda31cbcdf917c9a364619519797ee
DeepthiAddanki <deepthiiaddanki@gmail.com>
2026-03-05 20:17:24 +0530

    new changes

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql://root:DgGUqDCOpvSFrhSahqXCCSjrSTsaxNry@switchyard.proxy.rlwy.net:30200/railway"
+DATABASE_URL = "mysql+pymysql://root:DgGUqDCOpvSFrhSahqXCCSjrSTsaxNry@switchyard.proxy.rlwy.net:30200/railway"
 
 engine = create_engine(DATABASE_URL)
 
